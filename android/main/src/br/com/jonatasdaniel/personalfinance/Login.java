3072d0e5b5c1fb91d389723befcede0e34d9acd3
Jonatas Daniel <jonatas.hermann@gmail.com>
2011-03-08 21:26:06 -0300

    initial commit

---
+++ android/main/src/br/com/jonatasdaniel/personalfinance/Login.java
@@ -0,0 +1,27 @@
+package br.com.jonatasdaniel.personalfinance;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends Activity {
+    
+	private EditText edtUsuario;
+	private EditText edtSenha;
+	private Button btnEntrar;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        
+        init();
+    }
+    
+    private void init() {
+    	edtUsuario = (EditText) findViewById(R.login.edt_usuario);
+    	edtSenha = (EditText) findViewById(R.login.edt_senha);
+    	btnEntrar = (Button) findViewById(R.login.btn_logar);
+    }
+}
